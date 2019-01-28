8d0042ac46793bd28058cd70245b77b2be72e860
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-01-28 16:32:34 +0800

    Add transcription editing and finegrain zoom

--- app/components/wave-visualiser.js
+++ app/components/wave-visualiser.js
@@ -2,6 +2,289 @@ import Component from '@ember/component';
 import WaveformPlaylist from 'npm:waveform-playlist';
 export default Component.extend({
   didInsertElement(){
+    var notes = [
+        {
+          "begin": "2.29",
+          "children": [],
+          "end": "5.09",
+          "id":"0",
+          "language":"eng",
+          "lines":["a senior this important to make school"]
+        },{
+        "begin": "5.53",
+        "children": [],
+        "end": "10.92",
+        "id":"1",
+        "language":"eng",
+        "lines":["and when school is fun you don't get kids waking up in the morning i say i'll do i have to come to school"]
+      },{
+        "begin": "11.01",
+        "children": [],
+        "end": "16.35",
+        "id":"2",
+        "language":"eng",
+        "lines":["so we embrace technology because it helps us make learning more"]
+      },{
+        "begin": "16.48",
+        "children": [],
+        "end": "21.57",
+        "id":"3",
+        "language":"eng",
+        "lines":["i believe that when kids are engaged when kids are interested that's where learning takes"]
+      },{
+        "begin": "23.09",
+        "children": [],
+        "end": "24.76",
+        "id":"4",
+        "language":"eng",
+        "lines":[""]
+      },{
+        "begin": "24.76",
+        "children": [],
+        "end": "35.26",
+        "id":"5",
+        "language":"eng",
+        "lines":["<unk> kits are relieved from a very different world now i really to reach out to the kids you need to be savvy with technology if you're not savvy with technology you're going to lose to kids in the"]
+      },{
+        "begin": "35.28",
+        "children": [],
+        "end": "38.79",
+        "id":"6",
+        "language":"eng",
+        "lines":[""]
+      },{
+        "begin": "38.79",
+        "children": [],
+        "end": "50.62",
+        "id":"7",
+        "language":"eng",
+        "lines":["yeah secondary school is a typical school in singapore where we take in students with different academic abilities and uh we have about one thousand five hundred and twelve students into"]
+      },{
+        "begin": "51.06",
+        "children": [],
+        "end": "66.13",
+        "id":"8",
+        "language":"eng",
+        "lines":["and i and they all come from the neighbourhood we get students from the age of thirteen to about sixteen many students want to come to this school because of its strong a programme especially in the use of i c t infocomm"]
+      },{
+        "begin": "66.45",
+        "children": [],
+        "end": "70.73",
+        "id":"9",
+        "language":"eng",
+        "lines":["and the teachers here are known to produce a very good teaching"]
+      },{
+        "begin": "70.96",
+        "children": [],
+        "end": "75.5",
+        "id":"10",
+        "language":"eng",
+        "lines":["and a very innovative teaching ideas to engage the students in the"]
+      },{
+        "begin": "75.5",
+        "children": [],
+        "end": "79.9",
+        "id":"11",
+        "language":"eng",
+        "lines":[""]
+      },{
+        "begin": "79.9",
+        "children": [],
+        "end": "98.68",
+        "id":"12",
+        "language":"eng",
+        "lines":["in the early nineteen nineties the teachers really are the monopoly of knowledge and they are the one that comes to the glass to deliver that knowledge so that the students can acquire them but today knowledge is no more a monopoly amanda details because students can get knowledge from a myriad of"]
+      },{
+        "begin": "98.68",
+        "children": [],
+        "end": "102.15",
+        "id":"13",
+        "language":"eng",
+        "lines":["and hence the role of the teacher"]
+      },{
+        "begin": "102.15",
+        "children": [],
+        "end": "105.42",
+        "id":"14",
+        "language":"eng",
+        "lines":["facilitation dummies facilitate"]
+      },{
+        "begin": "105.42",
+        "children": [],
+        "end": "112.27",
+        "id":"15",
+        "language":"eng",
+        "lines":["where they could get the right knowledge how they could synthesize them how they could discern the information that they"]
+      },{
+        "begin": "112.27",
+        "children": [],
+        "end": "120.55",
+        "id":"16",
+        "language":"eng",
+        "lines":["velocity defies the rate of change of distance with time is it true or false <v-noise> so i want you to treat me so this is how you do it this is a format they need to"]
+      },{
+        "begin": "120.62",
+        "children": [],
+        "end": "123.83",
+        "id":"17",
+        "language":"eng",
+        "lines":["so you what is at sign vote by to eat"]
+      },{
+        "begin": "123.83",
+        "children": [],
+        "end": "133.0",
+        "id":"18",
+        "language":"eng",
+        "lines":["we look at technology very meaningful and we see how can we leverage this technology to make a very significant impact in the classroom"]
+      },{
+        "begin": "133.0",
+        "children": [],
+        "end": "144.68",
+        "id":"19",
+        "language":"eng",
+        "lines":["i give you an example in a classroom of forty it is really impossible to get forty students to us forty questions at one gulp when we use the instant messaging tool we opened forty windows to faulty"]
+      },{
+        "begin": "144.68",
+        "children": [],
+        "end": "153.3",
+        "id":"20",
+        "language":"eng",
+        "lines":["they could ask forty questions at the same time and a teacher could see their thinking on uh on the technology to that they use and kids getting what excited because they are"]
+      },{
+        "begin": "153.3",
+        "children": [],
+        "end": "158.48",
+        "id":"21",
+        "language":"eng",
+        "lines":["the tools that they are very very good in using not just a pen and"]
+      },{
+        "begin": "158.48",
+        "children": [],
+        "end": "168.02",
+        "id":"22",
+        "language":"eng",
+        "lines":["okay so since then we will have a two students were avid let me know if you have any issues or that the meals raise your hand right yeah okay let's"]
+      },{
+        "begin": "168.02",
+        "children": [],
+        "end": "183.65",
+        "id":"23",
+        "language":"eng",
+        "lines":["what the students are doing is currently exploring this second life art gallery which to swear set up and the works that are shown in this together he is actually made up of our local works they have been done by local"]
+      },{
+        "begin": "183.65",
+        "children": [],
+        "end": "189.63",
+        "id":"24",
+        "language":"eng",
+        "lines":[""]
+      },{
+        "begin": "189.63",
+        "children": [],
+        "end": "208.18",
+        "id":"25",
+        "language":"eng",
+        "lines":["because of all the online platform is very very useful because uh it's something that really engaging the students they will be chatting with one another i care about the works using the enemies of our principles of design as well as a reading other students comments as well if you'd like to decay actually leave notes for other students should"]
+      },{
+        "begin": "208.18",
+        "children": [],
+        "end": "212.33",
+        "id":"26",
+        "language":"eng",
+        "lines":[""]
+      },{
+        "begin": "214.68",
+        "children": [],
+        "end": "229.67",
+        "id":"27",
+        "language":"eng",
+        "lines":["web two point zero worked with wiki with your facebook your blocks you find that uh that is a very participate tree culture it calls for a lot of collaboration they no longer become just a consumer of knowledge they actually produce"]
+      },{
+        "begin": "229.91",
+        "children": [],
+        "end": "236.87",
+        "id":"28",
+        "language":"eng",
+        "lines":["discovered so"]
+      },{
+        "begin": "236.87",
+        "children": [],
+        "end": "251.9",
+        "id":"29",
+        "language":"eng",
+        "lines":["i find that students themselves are often on facebook so instead of looking at them and at looking at facebook s with destruction i would rather use it to engage them they even like when they're stuck with southern question daily pulls up the question to the class and you see"]
+      },{
+        "begin": "252.07",
+        "children": [],
+        "end": "259.81",
+        "id":"30",
+        "language":"eng",
+        "lines":["and they are learning from each other which i think it's better because this more motivation intrinsic motivation for them to learn from"]
+      },{
+        "begin": "263.32",
+        "children": [],
+        "end": "270.43",
+        "id":"31",
+        "language":"eng",
+        "lines":["i would say that the teachers in the school myself included we we scanned the globe for best"]
+      }
+
+    ];
+    var actions = [
+      {
+        class: 'fa.fa-minus',
+        title: 'Reduce annotation end by 0.010s',
+        action: (annotation, i, annotations, opts) => {
+          var next;
+          var delta = 0.010;
+          annotation.end -= delta;
+
+          if (opts.linkEndpoints) {
+            next = annotations[i + 1];
+            next && (next.start -= delta);
+          }
+        }
+      },
+      {
+        class: 'fa.fa-plus',
+        title: 'Increase annotation end by 0.010s',
+        action: (annotation, i, annotations, opts) => {
+          var next;
+          var delta = 0.010;
+          annotation.end += delta;
+
+          if (opts.linkEndpoints) {
+            next = annotations[i + 1];
+            next && (next.start += delta);
+          }
+        }
+      },
+      {
+        class: 'fa.fa-scissors',
+        title: 'Split annotation in half',
+        action: (annotation, i, annotations) => {
+          const halfDuration = (annotation.end - annotation.start) / 2;
+
+          annotations.splice(i + 1, 0, {
+            id: 'test',
+            start: annotation.end - halfDuration,
+            end: annotation.end,
+            lines: ['----'],
+            lang: 'en',
+          });
+
+          annotation.end = annotation.start + halfDuration;
+        }
+      },
+      {
+        class: 'fa.fa-trash',
+        title: 'Delete annotation',
+        action: (annotation, i, annotations) => {
+          annotations.splice(i, 1);
+        }
+      }
+    ];
+
     var playlist = WaveformPlaylist.init({
       samplesPerPixel: 3000,
       waveHeight: 100,
@@ -17,8 +300,15 @@ export default Component.extend({
         show: true, //whether or not to include the track controls
         width: 200 //width of controls in pixels
       },
+      annotationList: {
+        annotations: notes,
+        controls: actions,
+        editable: true,
+        isContinuousPlay: false,
+        linkEndpoints: true
+      },
       seekStyle : 'line',
-      zoomLevels: [500, 1000, 3000, 5000]
+      zoomLevels: [25, 50, 100, 500, 1000, 3000, 5000]
     });
     console.log('playlist defined', playlist);
     playlist.load([
