3027754d6dbb0503086c023c09560ea7e8d88c41
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-03-04 11:43:45 +0800

    Temporarily comment out annotation actions

--- app/components/wave-visualiser.js
+++ app/components/wave-visualiser.js
@@ -147,58 +147,58 @@ export default Component.extend({
   actions:{ loadWaveFile(audioFile, notes) {
       this.set('isPlayerLoading', true);
       var actions = [
-        {
-          class: '.red.minus.icon',
-          title: 'Reduce annotation end by 0.010s',
-          action: (annotation, i, annotations, opts) => {
-            var next;
-            var delta = 0.010;
-            annotation.end -= delta;
-
-            if (opts.linkEndpoints) {
-              next = annotations[i + 1];
-              next && (next.start -= delta);
-            }
-          }
-        },
-        {
-          class: '.green.plus.icon',
-          title: 'Increase annotation end by 0.010s',
-          action: (annotation, i, annotations, opts) => {
-            var next;
-            var delta = 0.010;
-            annotation.end += delta;
-
-            if (opts.linkEndpoints) {
-              next = annotations[i + 1];
-              next && (next.start += delta);
-            }
-          }
-        },
-        {
-          class: '.red.scissors',
-          title: 'Split annotation in half',
-          action: (annotation, i, annotations) => {
-            const halfDuration = (annotation.end - annotation.start) / 2;
-
-            annotations.splice(i + 1, 0, {
-              id: 'test',
-              start: annotation.end - halfDuration,
-              end: annotation.end,
-              lines: ['----'],
-              lang: 'en',
-            });
-
-            annotation.end = annotation.start + halfDuration;
-          }
-        },
-        {
-          class: 'fa.fa-trash',
-          title: 'Delete annotation',
-          action: (annotation, i, annotations) => {
-            annotations.splice(i, 1);
-          }
-        }
+        // {
+        //   class: '.red.minus.icon',
+        //   title: 'Reduce annotation end by 0.010s',
+        //   action: (annotation, i, annotations, opts) => {
+        //     var next;
+        //     var delta = 0.010;
+        //     annotation.end -= delta;
+        //
+        //     if (opts.linkEndpoints) {
+        //       next = annotations[i + 1];
+        //       next && (next.start -= delta);
+        //     }
+        //   }
+        // },
+        // {
+        //   class: '.green.plus.icon',
+        //   title: 'Increase annotation end by 0.010s',
+        //   action: (annotation, i, annotations, opts) => {
+        //     var next;
+        //     var delta = 0.010;
+        //     annotation.end += delta;
+        //
+        //     if (opts.linkEndpoints) {
+        //       next = annotations[i + 1];
+        //       next && (next.start += delta);
+        //     }
+        //   }
+        // },
+        // {
+        //   class: '.red.scissors',
+        //   title: 'Split annotation in half',
+        //   action: (annotation, i, annotations) => {
+        //     const halfDuration = (annotation.end - annotation.start) / 2;
+        //
+        //     annotations.splice(i + 1, 0, {
+        //       id: 'test',
+        //       start: annotation.end - halfDuration,
+        //       end: annotation.end,
+        //       lines: ['----'],
+        //       lang: 'en',
+        //     });
+        //
+        //     annotation.end = annotation.start + halfDuration;
+        //   }
+        // },
+        // {
+        //   class: 'fa.fa-trash',
+        //   title: 'Delete annotation',
+        //   action: (annotation, i, annotations) => {
+        //     annotations.splice(i, 1);
+        //   }
+        // }
       ];
 
       var playlist = WaveformPlaylist.init({
