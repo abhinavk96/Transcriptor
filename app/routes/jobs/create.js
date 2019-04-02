1f08678d45e6742073b7b8c04a7ce3577a0ed95c
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-02 15:12:58 +0800

    Add model for jobs route

--- app/routes/jobs/create.js
+++ app/routes/jobs/create.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  authManager: service(),
+  session: service(),
+  async model(){
+    let transcriptions = await this.get('authManager.currentUser').get('transcriptions');
+    let currentUser= await this.get('authManager.currentUser');
+    let availableUsers = this.store.findAll('user');
+    return {
+      transcriptions,
+      creator: currentUser,
+      editors: availableUsers
+    }
+  }
 });
