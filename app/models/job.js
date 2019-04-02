b4499b4e8d5636a6de71d6f80adbf500a4229e5e
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-02 15:12:41 +0800

    Populate job model

--- app/models/job.js
+++ app/models/job.js
@@ -1,5 +1,21 @@
 import DS from 'ember-data';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
+  /** attributes
+   *
+   */
+  status: attr('string'),
+  rating: attr('string'),
+  createdAt: attr('moment', { readOnly: true }),
+
+  /** relationships
+   *
+   */
+  creator: belongsTo('user'),
+  transcription: belongsTo('transcription'),
+  role: belongsTo('role'),
+  assignee: belongsTo('user')
 
 });
