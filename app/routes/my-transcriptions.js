9e816feb0e73a2609e1cb9090c08a9f9063a827e
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-05 03:00:24 +0800

    FIX THE EDITORS ROUTEgit add app/templates/my-transcriptions/list.hbs git add app/templates/my-transcriptions/list.hbs

--- app/routes/my-transcriptions.js
+++ app/routes/my-transcriptions.js
@@ -3,8 +3,5 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend({
   authManager: service(),
-  session: service(),
-  model() {
-    return this.get('authManager.currentUser').get('transcriptions');
-  }
+  session: service()
 });
--- app/routes/my-transcriptions/list.js
+++ app/routes/my-transcriptions/list.js
@@ -1,10 +1,36 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import EmberObject from '@ember/object';
+import { A } from '@ember/array';
 
 export default Route.extend({
   authManager: service(),
   session: service(),
-  model(params) {
-    return this.get('authManager.currentUser').get('transcriptions');
+  loader: service(),
+  async model(params) {
+    const MyObject = EmberObject.extend({
+      // empty object
+    });
+    var result = [];
+
+    console.log(params);
+    if(params.transcription_type==='created') {
+      return this.get('authManager.currentUser').get('transcriptions');
+    }
+    if(params.transcription_type==='assigned') {
+      let data = await this.get('loader').load(`/users/${this.get('authManager.currentUser.id')}/editor-transcriptions`);
+      // return this.store.c('transcription', data);
+
+      data.data.forEach(item => {
+        item.id = null;
+        var model = this.store.createRecord('transcription', item.attributes);
+        result.addObject(model);
+
+      });
+      this.get('authManager.currentUser').set('editorTranscriptions', result);
+      return this.get('authManager.currentUser').get('editorTranscriptions');
+
+    }
+
   }
 });
