aeadb28115d7307e9e208b2821e1b3cbb4493f82
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-05 12:03:32 +0800

    Add drag-hover event to wizard

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -1,4 +1,32 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didInsertElement() {
+    this._super(...arguments);
+    this.$()
+      .on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+      })
+      .on('dragover dragenter', () => {
+        // this.$('#file-upload').addClass('basic');
+
+        this.$('#file-upload').addClass('orange');
+        this.$('#file-upload').removeClass('piled');
+
+        console.log('hover', this.$('.file-upload'));
+
+      })
+      .on('dragleave dragend drop', () => {
+        this.$('#file-upload').addClass('piled');
+
+        this.$('#file-upload').removeClass('orange');
+        console.log('hover');
+      })
+      .on('drop', e => {
+        // this.processFiles(e.originalEvent.dataTransfer.files);
+        console.log('dropped');
+      });
+  }
+
 });
