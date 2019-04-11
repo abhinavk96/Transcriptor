318bed860747ca1a65af1e4c033de5a7194c77c9
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-11 14:52:16 +0800

    Add new fields to transcription model

--- app/models/transcription.js
+++ app/models/transcription.js
@@ -5,6 +5,8 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 export default DS.Model.extend({
   name: attr('string'),
   description: attr('string'),
+  status:attr('string'),
+  asrName: attr('string'),
   fileAddress: attr('string'),
   createdAt: attr('moment', { readOnly: true }),
   audioDuration: attr('string'),
