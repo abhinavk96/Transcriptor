17104fb149e3004b8e4476c3da71d514422ae645
ritikamotwani <ritika.motwani0712@gmail.com>
2019-04-12 16:59:17 +0800

    Abhinav is a panda genius

--- app/components/transcription-card.js
+++ app/components/transcription-card.js
@@ -1,15 +1,24 @@
 import Component from '@ember/component';
+import { later } from '@ember/runloop';
 
 export default Component.extend({
   classNames: ['ui', 'item'],
+  isLoading: false,
   actions: {
     redirectCreateJob(data) {
       this.redirectSuccess(data);
       console.log('Switching to create job route');
     },
     refreshStatus(status, model) {
-      model.reload();
+      this.set('isLoading', true);
+      later( () =>{
+        model.reload()
+        .then(() => {
+          this.set('isLoading', false);
+        });
+      }, 2000);
       console.log(status);
+      console.log($(event.srcElement).transition('pulse'));
     }
   }
 });
