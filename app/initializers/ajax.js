8c5e768b339189d6dfbb2b2b9e51d5f529f68441
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-17 19:13:33 +0800

    Remove typo

--- app/initializers/ajax.js
+++ app/initializers/ajax.js
@@ -8,18 +8,3 @@ export default {
 };
 
 
-
-
-recID > session 1 > b1.WAV, Cei1.wav, close1.WAV
-        session 2 > b2.WAV, ceil2.wav
-
-meta.json
-
-API CALL
-
-output > recid > session1 > boundary > 1.2.stm, 1.2.ctm
-                            closetalk > stm. ctm
-
-
-
-Categorise by session
