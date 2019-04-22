b91886768995cdde617a1c23b8be6514edfc8b7c
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-22 14:57:45 +0800

    Add unauthenticated rotue mixin to waveform editor

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(UnauthenticatedRouteMixin, {
   session : service(),
   model() {
     return this.store.findAll('user');
