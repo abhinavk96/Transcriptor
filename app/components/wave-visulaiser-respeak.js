ad8af7bbc131cdd21d6f30a64694828b51e57da0
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-24 15:21:44 +0800

    Highlight currently playing segment

--- app/components/wave-visulaiser-respeak.js
+++ app/components/wave-visulaiser-respeak.js
@@ -10,7 +10,22 @@ export default Component.extend({
     this._super(...arguments);
     this.send('loadWaveFile');
   },
+
+  currentSegment: null,
+  segmentTimes: [],
+
   actions: {
+
+    setCurrentSegment(time) {
+      this.segmentTimes.forEach((times, index) => {
+        if(time>=times.start && time<=times.end) {
+          if(this.currentSegment !== index)
+          this.set('currentSegment', index);
+          $('.segment.box').removeClass('current');
+          $('.segment.box').eq(index).addClass('current');
+        }
+      })
+    },
     loadWaveFile() {
       this.set('isPlayerLoading', true);
       var playlist = WaveformPlaylist.init({
@@ -31,6 +46,8 @@ export default Component.extend({
           "name": "Vocals"
         }
       ]).then(() => {
+        const ee = playlist.getEventEmitter();
+
         let duration = parseFloat(playlist.duration);
         $container.on("click", ".btn-play", function () {
           ee.emit("play");
@@ -39,8 +56,10 @@ export default Component.extend({
         let timePixel =(parseFloat(waveFormOuterWidth/duration).toFixed(2));
         let segmentBoxes = [];
           $('#segment-container').css({"width": waveFormOuterWidth + "px"});
+          let startTimeSegments = [];
         this.data.segmentsList.forEach((segment, index) => {
           console.log(segment._attributes);
+          startTimeSegments.push({'start' :parseFloat(segment._attributes.stime), 'end': parseFloat(segment._attributes.stime) + parseFloat(segment._attributes.dur)});
           let segmentBox = document.createElement('div');
           segmentBox.classList.add("segment", "box");
           segmentBox.innerHTML = index+1;
@@ -51,12 +70,23 @@ export default Component.extend({
 
         });
         console.log(segmentBoxes);
+        this.set('segmentTimes', startTimeSegments);
         segmentBoxes.forEach(segmentBox => {
           $('#segment-container').append(segmentBox);
         })
         //can do stuff with the playlist.
+        const updateTime = time => {
+          this.set('currentTime', time);
+          this.send('setCurrentSegment', time);
+        };
+        ee.on("timeupdate", updateTime);
+        ee.emit('play');
       });
 
+
+
+
     }
-  }
+  },
+
 });
