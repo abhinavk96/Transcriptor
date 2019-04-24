ca276a0d354539a968df7892a5a711aae480a8d5
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-04-24 15:43:13 +0800

    Add recorder js functions

--- app/routes/respeak/list.js
+++ app/routes/respeak/list.js
@@ -1,6 +1,38 @@
 import Route from '@ember/routing/route';
 import convert from 'npm:xml-js';
+import { inject } from '@ember/service';
+
 export default Route.extend({
+  recorder: inject(),
+  init() {
+    let recorder = this.get('recorder');
+  },
+  setupController(controller) {
+    this._super(...arguments);
+    let recorder = this.get('recorder');
+    recorder.set('recordingTime', 5000);
+    controller.set('recorder', recorder);
+  },
+  actions: {
+    async record() {
+      console.log('Record')
+      let recorder = this.get('recorder');
+      await recorder.start();
+
+      let { base64, audioURL, blob } = await recorder.getAudio();
+      console.log(base64, audioURL, blob);
+    },
+    async play() {
+      let recorder = this.get('recorder');
+      recorder.play();
+    },
+    async stop() {
+      let recorder = this.get('recorder');
+      recorder.stop();
+      recorder.close(); // Cloase audio context
+    }
+  },
+
   async model(params) {
     console.log(params);
     const transcription = await this.store.findRecord('transcription', params.transcription_id);
