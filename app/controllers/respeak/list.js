8918f52610632b49e8e6697de87ccb6096dae9b2
ritikamotwani <ritika.motwani0712@gmail.com>
2019-05-10 11:50:49 +0800

    Add modal for instructions in respeak

--- app/controllers/respeak/list.js
+++ app/controllers/respeak/list.js
@@ -4,6 +4,7 @@ export default Controller.extend({
   modalOffset: 200,
   actions: {
     openModal: function(name) {
+      //console.log(name);
       $('.ui.' + name + '.modal').modal({ detachable:false, observeChanges:true, offset:this.modalOffset }).modal('show').modal('refresh');
     }
   }
