001cd02411180a45d13bad6f368cc72df67461d7
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-05-16 15:30:27 +0800

    Add upload method to respeak

--- app/controllers/respeak/list.js
+++ app/controllers/respeak/list.js
@@ -6,6 +6,26 @@ export default Controller.extend({
     openModal: function(name) {
       //console.log(name);
       $('.ui.' + name + '.modal').modal({ detachable:false, observeChanges:true, offset:this.modalOffset }).modal('show').modal('refresh');
+    },
+    uploadFiles() {
+      console.log(this.audioFileArray);
+      var xhr = new XMLHttpRequest();
+      var formData = new FormData();
+      xhr.onload = (r)=>{
+        console.log('sent',r);
+      };
+      xhr.open("POST", "http://localhost:5000/upload/files/multi", true);
+      xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+      for(var file in this.audioFileArray) {
+        if(this.audioFileArray[file].src) {
+          let fileBlob=this.audioFileArray[file].blob;
+          fileBlob.lastModifiedDate = new Date();
+          fileBlob.name = `${file}.wav`;
+          formData.append("files[]", fileBlob);
+          console.log(formData);
+        }
+      }
+      xhr.send(formData);
     }
   }
 });
