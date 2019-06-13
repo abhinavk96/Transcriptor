3f9b9274d38660f0bd79303b48c2e6b75a10bf0f
CosmicCoder96 <abhinav.khare31@gmail.com>
2019-06-13 10:33:34 +0800

    Use notify on users page to save/update records

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -1,15 +1,19 @@
 import Controller from '@ember/controller';
-
+import { inject as service} from '@ember/service';
 export default Controller.extend({
+  notify: service(),
   actions : {
     save(user) {
       console.log(user.email);
       user.save()
         .then(savedUser=> {
           console.log('user saved', savedUser.isAdmin)
+          this.notify.success(`User ${user.email} access status updated` );
+
         })
         .catch(e=> {
           console.warn(e)
+          this.notify('An unexpected error occurred. Check dev console.')
         })
     }
   }
